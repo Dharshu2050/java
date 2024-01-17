da960b817920015c85a31ef90cd78b12e0959753
Dharshu2050 <dharshinieaswaran52@gmail.com>
2024-01-17 19:19:44 +0530

    c

---
+++ Java Practice/b.java
@@ -0,0 +1,35 @@
+/**
+ * b
+ 
+public class b {
+
+    public static void main(String[] args) {
+        int a=10;
+        char ch=A;
+        float f=8.9f;
+        String str="VCET";
+        System.out.println(str+str);
+        System.out.println(a+ch+f+str);
+        System.out.println(""+a+ch+f+str);
+        System.out.println(ch+2);
+        System.out.println(ch+""+2);
+        System.out.println(""+ch+2);
+    }
+}
+*/
+/**
+ * b
+ 
+public class b {
+
+    public static void main(String[] args) {
+        System.out.print("VCET");
+        System.out.println("VCET");
+        System.out.print("VCET");
+        System.out.print("VCET");
+    }
+}*/
+
+/**
+ * b
+ */
---
+++ Java Practice/c.java
@@ -0,0 +1,51 @@
+/*  
+import java.util.Scanner;
+import java.io.*;
+public class c{
+
+    public static void main(String[] args) {
+       
+        Scanner sc=new Scanner(System.in);
+        String name=sc.nextLine();
+        System.out.println("name:"+name);
+        int age=sc.nextInt();
+        sc.nextLine();
+        System.out.println("age:"+age);
+        String rollno=sc.nextLine();
+        System.out.println("rollno:"+rollno);
+        float cgpa=sc.nextFloat();
+        System.out.println("cgpa:"+cgpa);
+        String gender=sc.nextLine();
+        System.out.println("gender:"+gender);
+
+        
+
+    }
+}
+    */ 
+
+import java.util.Scanner;
+import java.io.*;
+public class c {
+
+    public static void main(String[] args) {
+       
+        Scanner sc=new Scanner(System.in);
+        
+        System.out.print("name:");
+        String name=sc.nextLine();
+        System.out.print("age:");
+        int age=sc.nextInt();
+        sc.nextLine();
+        System.out.println("rollno:");
+        String rollno=sc.nextLine();
+        System.out.println("cgpa:");
+        float CGPA=sc.nextFloat();
+       System.out.println("gender:");
+        String gender=sc.nextLine();
+        
+
+        
+
+    }
+}
