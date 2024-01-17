2555577e26a24c47139f0f21ac735820e64c6d73
Dharshu2050 <dharshinieaswaran52@gmail.com>
2024-01-17 21:30:07 +0530

    d

---
+++ Java Practice/d.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+public class d {
+    public static void main(String[] args) {
+        
+       int a=125;
+       int b=24;
+        int sum=a+b;
+        int sum1=a-b;
+        int sum2=a*b;
+        float sum3=a/b;
+        int sum4=a%b;
+        System.out.println(sum);
+        System.out.println(sum1);
+        System.out.println(sum2);
+        System.out.println(sum3);
+        System.out.println(sum4);
+
+
+
+        
+        
+        
+
+
+    }
+}
---
+++ Java Practice/seconds.java
@@ -0,0 +1,14 @@
+public class seconds {
+    public static void main(String[] args) {
+        
+      int s3=86399/3600;
+      int s1=86399/60;
+      int s2=86399/60;
+      System.out.print(s3);
+      System.out.print(s1);
+      System.out.print(s2);
+
+
+    }
+    
+}
