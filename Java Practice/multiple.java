f7b9f98a822377792e86ded1ff1ff46fbfa59571
Dharshu2050 <dharshinieaswaran52@gmail.com>
2024-02-08 19:55:04 +0530

    java

---
+++ Java Practice/multiple.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.io.*;
+
+public class multiple {
+    Scanner sc=new Scanner(System.in);
+    float f1=sc.nextFloat();
+    System.out.println();
+    
+
+
+}
