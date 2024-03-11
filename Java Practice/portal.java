01c418e11002c33a8732a0c93eeed59ba7a27658
Dharshu2050 <dharshinieaswaran52@gmail.com>
2024-03-11 20:41:01 +0530

    pro

---
+++ Java Practice/portal.java
@@ -0,0 +1,14 @@
+public class portal {
+    public static void main(String[] args) {
+	    for(int i=0;i<5;i++){
+		System.out.println("Hello World");
+		i++;
+		i--;
+	    }
+	    int a=10;
+	   // System.out.System.out.println(++a++);
+
+       
+
+}
+}
---
+++ Java Practice/pro.java
@@ -0,0 +1,9 @@
+public class pro {
+    public static void main(String[] args) {
+        for(int i=0;i<5;){
+            System.out.println("Hello World");
+            i++;
+    }
+}
+
+}
