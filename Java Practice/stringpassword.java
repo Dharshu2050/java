ef5878006c3651301177aa4002d0b129002f95fe
Dharshu2050 <dharshinieaswaran52@gmail.com>
2024-03-16 23:16:10 +0530

    javapratice

---
+++ Java Practice/stringpassword.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class stringpassword {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+
+        int z=(a/1000)%10;
+        int x=(b/1000)%10;
+        
+
+    }
+}
--- Java Practice/substr.java
+++ Java Practice/substr.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class substr {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+
+        //get two inputs and one string ,substring find eg : 2 to 6 string hello world o/p:ring h
         int a=sc.nextInt();
         sc.nextLine();
 
