b2a527c357a6e83065373f5c8458f735df5d7967
Dharshu2050 <dharshinieaswaran52@gmail.com>
2024-03-16 23:22:01 +0530

    java

--- Java Practice/character.java
+++ Java Practice/character.java
@@ -3,10 +3,11 @@ import java.util.Scanner;
 public class character {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        // n th character of string  eg:vcet position :3 o/p :t
+        // n th character of string  eg:vcet position :3 
         String str=sc.nextLine();
         int n=sc.nextInt();
         int index=str.charAt(n);
          System.out.println((char)index);
+         // o/p :t
     }
 }
