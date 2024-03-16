a993cc694fa46a97d4f1289c78813c0a4d1704bd
Dharshu2050 <dharshinieaswaran52@gmail.com>
2024-03-16 23:25:24 +0530

    java

--- Java Practice/coins7.java
+++ Java Practice/coins7.java
@@ -4,6 +4,9 @@ import java.util.*;
 public class coins7 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+       
+        //add the poketmoney to get 10rs coins and 5rs coins o/p: for eg 10rs coins:5 10*5=50 5rs coins :5*5 =25 50+25=75  
+
         int a=10;
         int b=5;
         int c=2;
